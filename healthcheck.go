7fcff950bcce3882bb6b57754a7ec0bcb42bf380
TienNM5 <tiennm99@outlook.com>
2021-09-11 23:04:35 +0700

    [Add] simple communication test

---
+++ healthcheck.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+	"github.com/heroiclabs/nakama-common/runtime"
+)
+
+type HealCheckResponse struct {
+	Success bool `json:"success"`
+}
+
+func RpcHealthcheck(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	logger.Error("Healthcheck RPC called")
+	response := &HealCheckResponse{Success: true}
+	out, err := json.Marshal(response)
+	if err != nil {
+		logger.Error("Error marshalling response type to JSON: %v", err)
+		return "", runtime.NewError("Cannon marshal type", 13)
+	}
+
+	return string(out), nil
+}
