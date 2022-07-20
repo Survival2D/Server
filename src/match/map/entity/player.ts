dce43913f552fe7f3d0c5a2ce9f0891b4d2fb43b
TienNM99 <tiennm99@outlook.com>
2022-07-20 23:53:49 +0700

    [Remove] delete all for reinit

--- src/match/map/entity/player.ts
+++
@@ -1,9 +0,0 @@
-// class Player {
-//   id: string = "";
-//   hp: number = DEFAULT_HEATH_POINT; //heath point
-//   rotation: number = 0; // hướng xoay nhân vật
-//
-//   Player(id: string) {
-//     this.id = id;
-//   }
-// }
