6ef67fe8a2df72d6888bd53bdfb3535f1afc94dc
TienNM99 <tiennm99@outlook.com>
2022-07-26 20:05:16 +0700

    [Add] flow login

--- src/main/java/com/survival2d/server/controller/UserLoginController.java
+++ src/main/java/com/survival2d/server/controller/UserLoginController.java
@@ -2,23 +2,19 @@ package com.survival2d.server.controller;
 
 import static com.tvd12.ezyfoxserver.constant.EzyEventNames.USER_LOGIN;
 
-import com.survival2d.server.entity.GamePlayerId;
+import com.survival2d.server.config.LoginConfig;
 import com.survival2d.server.entity.User;
-import com.survival2d.server.repo.CredentialRepo;
 import com.survival2d.server.service.UserService;
 import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
 import com.tvd12.ezyfox.core.annotation.EzyEventHandler;
-import com.tvd12.ezyfox.entity.EzyArray;
 import com.tvd12.ezyfox.io.EzyStrings;
 import com.tvd12.ezyfox.sercurity.EzySHA256;
-import com.tvd12.ezyfoxserver.constant.EzyEventNames;
 import com.tvd12.ezyfoxserver.constant.EzyLoginError;
 import com.tvd12.ezyfoxserver.context.EzyPluginContext;
 import com.tvd12.ezyfoxserver.controller.EzyAbstractPluginEventController;
 import com.tvd12.ezyfoxserver.event.EzyUserLoginEvent;
 import com.tvd12.ezyfoxserver.exception.EzyLoginErrorException;
-import lombok.Setter;
 
 @EzySingleton
 @EzyEventHandler(USER_LOGIN)
@@ -31,56 +27,36 @@ public class UserLoginController extends EzyAbstractPluginEventController<EzyUse
     logger.info("{} login in", event.getUsername());
 
     String username = event.getUsername();
-    String password = encodePassword(event.getPassword());
-
-    User user = userService.getUser(username);
-
-    if (user == null) {
-      logger.info("User doesn't exist in db, create a new one!");
-      user = userService.createUser(username, password);
-      userService.saveUser(user);
+    if (EzyStrings.isNoContent(username)) {
+      throw new EzyLoginErrorException(EzyLoginError.INVALID_USERNAME);
     }
 
-    if (!user.getPassword().equals(password)) {
+    String password = event.getPassword();
+    if (LoginConfig.isEnableAuth && EzyStrings.isNoContent(password)) {
       throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
     }
 
-    logger.info("user and password match, accept user: {}", username);
+    String encodedPassword = encodePassword(password);
+    User user = userService.getUser(username);
+    if (user == null) {
+      logger.info("User doesn't exist in db, create a new one!");
+      user = userService.createUser(username, encodedPassword);
+      userService.saveUser(user);
+      return;
+    }
+
+    if (LoginConfig.isEnableAuth) {
+      if (!user.getPassword().equals(encodedPassword)) {
+        throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
+      }
+      logger.info("user and password match, accept user: {}", username);
+      return;
+    }
+
+    logger.info("Server not enable authentication, login directly");
   }
 
   private String encodePassword(String password) {
     return EzySHA256.cryptUtfToLowercase(password);
   }
 }
-//@Setter
-//@EzySingleton
-//@EzyEventHandler(EzyEventNames.USER_LOGIN)
-//public class UserLoginController
-//    extends EzyAbstractPluginEventController<EzyUserLoginEvent> {
-//
-//  @EzyAutoBind
-//  private CredentialRepo credentialRepo;
-//
-//  @Override
-//  public void handle(EzyPluginContext ctx, EzyUserLoginEvent event) {
-//    logger.info("Space Game - user {} login in", event.getUsername());
-//    String player = event.getUsername();
-//    if(EzyStrings.isNoContent(player))
-//      throw new EzyLoginErrorException(EzyLoginError.INVALID_USERNAME);
-//    String password = event.getPassword();
-//    if(EzyStrings.isNoContent(password))
-//      throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
-//    EzyArray data = event.getData();
-//    String game = data.get(0);
-//    GamePlayerId gamePlayerId = new GamePlayerId(game, player);
-//    Credential credential = credentialRepo.findById(gamePlayerId);
-//    String sha256Password = EzySHA256.cryptUtfToLowercase(password);
-//    if(credential != null) {
-//      if(!credential.getPassword().equals(sha256Password))
-//        throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
-//    }
-//    else {
-//      credentialRepo.save(new Credential(gamePlayerId, sha256Password));
-//    }
-//  }
-//}
