0b06c9c94831f793cd317df3683d379bad040249
tiennm99 <tiennm99@outlook.com>
2022-09-11 10:03:52 +0700

    [Change] upgrade lib, test jackson deserialize packet

--- src/main/java/com/survival2d/server/controller/server/UserLoginController.java
+++ src/main/java/com/survival2d/server/controller/server/UserLoginController.java
@@ -9,7 +9,7 @@ import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
 import com.tvd12.ezyfox.bean.annotation.EzySingleton;
 import com.tvd12.ezyfox.core.annotation.EzyEventHandler;
 import com.tvd12.ezyfox.io.EzyStrings;
-import com.tvd12.ezyfox.sercurity.EzySHA256;
+import com.tvd12.ezyfox.security.EzySHA256;
 import com.tvd12.ezyfoxserver.constant.EzyLoginError;
 import com.tvd12.ezyfoxserver.context.EzyPluginContext;
 import com.tvd12.ezyfoxserver.controller.EzyAbstractPluginEventController;
--- src/main/java/com/survival2d/server/request/reader/TestRequestReader.java
+++ src/main/java/com/survival2d/server/request/reader/TestRequestReader.java
@@ -1,5 +1,7 @@
 package com.survival2d.server.request.reader;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.survival2d.server.request.TestRequest;
 import com.tvd12.ezyfox.binding.EzyReader;
@@ -12,6 +14,12 @@ public class TestRequestReader implements EzyReader<Object, TestRequest> {
 
   @Override
   public TestRequest read(EzyUnmarshaller ezyUnmarshaller, Object o) {
+//    try {
+//      String content = o.toString();
+//      return new ObjectMapper().readValue(content, TestRequest.class);
+//    } catch (JsonProcessingException e) {
+//      throw new RuntimeException(e);
+//    }
     return new Gson().fromJson(o.toString(),TestRequest.class);
   }
 }
