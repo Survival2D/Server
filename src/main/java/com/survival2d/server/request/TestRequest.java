0a4e01fc22fa53415b4c48b9ca7ae6a475b07dbe
tiennm99 <tiennm99@outlook.com>
2022-09-11 09:50:26 +0700

    [Add] sample complex packet

--- src/main/java/com/survival2d/server/request/TestRequest.java
+++ src/main/java/com/survival2d/server/request/TestRequest.java
@@ -1,16 +1,39 @@
 package com.survival2d.server.request;
 
+import com.survival2d.server.match.Vector;
 import com.survival2d.server.request.entity.Object;
+import com.survival2d.server.request.reader.TestRequestReader;
+import com.tvd12.ezyfox.binding.EzyUnmarshaller;
+import com.tvd12.ezyfox.binding.annotation.EzyArrayBinding;
 import com.tvd12.ezyfox.binding.annotation.EzyObjectBinding;
+import com.tvd12.ezyfox.binding.annotation.EzyReader;
+import com.tvd12.ezyfox.binding.annotation.EzyReaderImpl;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@EzyObjectBinding
+//@EzyObjectBinding
 public class TestRequest {
 
   List<Integer> arr;
   Object obj;
+//@EzyReader(TestReader.class)
+  List<Test> tests;
+
+//  @EzyObjectBinding
+  static class Test {
+    String name = "empty";
+    Vector position = new Vector(0, 0);
+  }
+
+//  @EzyReaderImpl
+//  static class TestReader implements com.tvd12.ezyfox.binding.EzyReader<Object, Test> {
+//
+//    @Override
+//    public Test read(EzyUnmarshaller ezyUnmarshaller, Object object) {
+//      return new Test();
+//    }
+//  }
 }
--- src/main/java/com/survival2d/server/request/reader/TestRequestReader.java
+++ src/main/java/com/survival2d/server/request/reader/TestRequestReader.java
@@ -1,14 +1,17 @@
 package com.survival2d.server.request.reader;
 
+import com.google.gson.Gson;
 import com.survival2d.server.request.TestRequest;
 import com.tvd12.ezyfox.binding.EzyReader;
 import com.tvd12.ezyfox.binding.EzyUnmarshaller;
+import com.tvd12.ezyfox.binding.annotation.EzyReaderImpl;
+import com.tvd12.ezyfox.entity.EzyHashMap;
 
-// @EzyReaderImpl
+@EzyReaderImpl
 public class TestRequestReader implements EzyReader<Object, TestRequest> {
 
   @Override
   public TestRequest read(EzyUnmarshaller ezyUnmarshaller, Object o) {
-    return new TestRequest();
+    return new Gson().fromJson(o.toString(),TestRequest.class);
   }
 }
