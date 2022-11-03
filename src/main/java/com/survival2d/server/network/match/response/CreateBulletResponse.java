de79a8b29001f5cc98e4506626b716d4e352f62d
tiennm99 <tiennm99@outlook.com>
2022-11-03 20:32:13 +0700

    Update CreateBulletResponse.java

--- src/main/java/com/survival2d/server/network/match/response/CreateBulletResponse.java
+++ src/main/java/com/survival2d/server/network/match/response/CreateBulletResponse.java
@@ -1,18 +1,16 @@
 package com.survival2d.server.network.match.response;
 
-import com.survival2d.server.game.entity.Weapon;
+import com.survival2d.server.game.entity.Bullet;
 import com.tvd12.ezyfox.binding.annotation.EzyObjectBinding;
 import lombok.Builder;
 import lombok.Data;
 import org.locationtech.jts.math.Vector2D;
-
 
 @Data
 @Builder
 @EzyObjectBinding
 public class CreateBulletResponse {
 
-  private String username;
-  private Weapon weapon;
+  private Bullet bullet;
   private Vector2D direction;
 }
