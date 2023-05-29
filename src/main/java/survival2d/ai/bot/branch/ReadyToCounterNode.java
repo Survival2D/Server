a6988f8125ebf9074a67f914b6266955991deb2c
TienNM <tiennm99@outlook.com>
2023-05-29 19:26:37 +0700

    [fix[ fix logic bot btree when check bullets, remove check have gun

--- src/main/java/survival2d/ai/bot/branch/ReadyToCounterNode.java
+++ src/main/java/survival2d/ai/bot/branch/ReadyToCounterNode.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import survival2d.ai.bot.Bot;
 import survival2d.ai.bot.BotBehaviorNode;
 import survival2d.ai.bot.leaf.HaveEnoughBulletNode;
-import survival2d.ai.bot.leaf.HaveGunNode;
 import survival2d.ai.btree.BTNode;
 import survival2d.ai.btree.branch.BTSequenceNode;
 
@@ -12,13 +11,10 @@ public class ReadyToCounterNode extends BTSequenceNode {
   ReadyToCounterNode(Bot controller) {
     super();
 
-    BotBehaviorNode haveGun = new HaveGunNode();
-    haveGun.setController(controller);
     BotBehaviorNode haveBullet = new HaveEnoughBulletNode();
     haveBullet.setController(controller);
 
     ArrayList<BTNode> children = new ArrayList<>();
-    children.add(haveGun);
     children.add(haveBullet);
 
     this.setChildren(children);
